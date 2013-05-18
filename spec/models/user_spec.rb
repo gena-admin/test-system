c9d55bd6f7f15448a15b41d668103b092b6dca72
Zelenskiy Gennadiy <gena.admin@gmail.com>
2013-05-19 02:04:53 +0300

    added few user model rspec (TEST NOT WORK)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should create new user' do
+    expect {
+      FactoryGirl.create(:user)
+    }.to change { User.count }.by 1
+  end
+
+  context 'validation' do
+    context 'email' do
+      it { FactoryGirl.build(:user, :email => nil).should have(1).error_on(:user) }
+      it { FactoryGirl.build(:user, :email => '').should have(1).error_on(:user) }
+      it { FactoryGirl.build(:user, :email => 'asxasxas').should have(1).error_on(:user) }
+      it { FactoryGirl.build(:user, :email => '1111111').should have(1).error_on(:user) }
+
+    end
+
+    context 'password' do
+      it { FactoryGirl.build(:user, :password => nil).should have(1).error_on(:user) }
+      it { FactoryGirl.build(:user, :password => '').should have(1).error_on(:user) }
+      it { FactoryGirl.build(:user, :password => 'sdc').should have(0).error_on(:user) }
+      it { FactoryGirl.build(:user, :password => '22').should have(0).error_on(:user) }
+    end
+  end
+
+
+  pending "add some test for format email and password length"
+
 end

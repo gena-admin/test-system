bf57d8cee786c300bf823d7d83c00f62a8113ef5
Surzhko Yevhen <SurzhkoYevhen@gmail.com>
2013-05-21 22:24:33 +0300

    implemented Quiz#next_answer method

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,9 +1,29 @@
 class Quiz < ActiveRecord::Base
   belongs_to :user
+  has_many :answers, :dependent => :destroy
   attr_accessible :finished_at
   validates :user, :presence => true
 
   def finished?
     !finished_at
+  end
+
+  def next_answer!
+    question_ids = answers.map(&:question_id)
+    result = answers.create
+    result.question = random_question question_ids
+    result
+  end
+
+  private
+
+  def random_question exclude_question_ids
+    if exclude_question_ids.empty?
+      questions_offset = rand Question.count
+      Question.first(:offset => questions_offset)
+    else
+      questions_offset = rand Question.where('id NOT IN (?)', exclude_question_ids).count
+      Question.where('id NOT IN (?)', exclude_question_ids).first(:offset => questions_offset)
+    end
   end
 end
--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -13,4 +13,37 @@ describe Quiz do
       it { FactoryGirl.build(:quiz, :user_id => 0).should have(1).error_on(:user) }
     end
   end
+
+  context '#next_question!' do
+    let(:quiz) { FactoryGirl.create(:quiz) }
+
+    context 'for one available question' do
+      let!(:question) { FactoryGirl.create(:question) }
+      let(:answer) { quiz.next_answer! }
+
+      it { answer.quiz.should == quiz }
+      it { answer.question.should == question }
+    end
+
+    context 'for one available question and with one answer' do
+      let!(:question) { FactoryGirl.create(:question) }
+      before { FactoryGirl.create(:answer, :quiz => quiz) }
+      let(:answer) { quiz.next_answer! }
+
+      it { answer.quiz.should == quiz }
+      it { answer.question.should == question }
+    end
+
+    context 'for one available question and with one answer' do
+      let!(:questions) do
+        result = []
+        5.times { result << FactoryGirl.create(:question) }
+        result
+      end
+      let(:answer) { quiz.next_answer! }
+
+      it { answer.quiz.should == quiz }
+      it { questions.should include answer.question }
+    end
+  end
 end

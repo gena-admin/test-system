99d8760c4bcc849a411c75e9df3ec92febe0d693
Zelenskiy Gennadiy <gena.admin@gmail.com>
2013-05-23 20:40:39 +0300

    added style and helper and other

--- app/helpers/attack/questions_helper.rb
+++ app/helpers/attack/questions_helper.rb
@@ -1,2 +1,24 @@
 module Attack::QuestionsHelper
+  ROWS_COUNT = 6
+  COLS_COUNT = 13
+
+  def choice_class choice
+    'success' if choice.is_correct
+  end
+
+  def question_to_matrix question
+     matrix = Array.new(ROWS_COUNT) { Array.new(COLS_COUNT) }
+     question.positions.each do |position|
+       matrix[position.dy - 1][position.dx + 6] = position
+     end
+     matrix
+  end
+
+  def position_style position
+    return if position.nil?
+    style = "color:##{position.color};"
+    style << "background-color: yellow" if position.is_highlighted
+    style
+  end
 end
+
